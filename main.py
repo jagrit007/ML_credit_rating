b3827295ae778c7f0645d3c02ad778b617aaa15a
Jagrit Thapar <jagritthapar007@gmail.com>
2024-04-25 17:55:53 +0000

    update: fastapi dataclass and comment out redundant fields for now

--- main.py
+++ main.py
@@ -16,20 +16,20 @@ class InputData(BaseModel):
     Employment: str
     Install_rate: int
     Marital_status: str
-    Co_applicant: str
-    Present_Resident: int
+    # Co_applicant: str
+    # Present_Resident: int
     Real_Estate: str
     Age: int
     Other_installment: str
-    Residence: str
+    # Residence: str
     Num_Credits: int
     Job: str
-    No_dependents: int
+    # No_dependents: int
     Phone: str
     Foreign: str
 
 # Load the serialized model
-with open('model.pkl', 'rb') as file:
+with open('model_new.pkl', 'rb') as file:
     model = pickle.load(file)
 
 # Initialize FastAPI app
@@ -37,7 +37,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Set this to your frontend URL in production
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST"],
     allow_headers=["*"],
@@ -50,10 +50,10 @@ def mapper(data):
     balance_in_savings_ac = {'less1000DM':0, 'less100DM':1, 'less500DM':2, 'over1000DM':3, 'unknown':4}
     employment = {'four-years':0, 'one-year':1, 'over-seven':2, 'seven-years':3, 'unemployed':4}
     marital_status = {'female-divorced': 0, 'male-divorced': 1, 'married-male':2, 'single-male':3}
-    co_applicant = {'co-applicant':0, 'guarantor':1, 'none':2}
+    # co_applicant = {'co-applicant':0, 'guarantor':1, 'none':2}
     real_estate = {'building-society':0, 'car':1, 'none':2, 'real-estate':3}
     other_installment = {'bank':0, 'none':1, 'stores':2}
-    residence = {'free':0, 'own':1, 'rent':2}
+    # residence = {'free':0, 'own':1, 'rent':2}
     job = {'management':0, 'skilled':1, 'unemployed-non-resident':2, 'unskilled-resident':3}
     phone = {'no':0, 'yes':1}
     foreign = {'no':0, 'yes':1}
@@ -66,15 +66,15 @@ def mapper(data):
             employment[data.Employment], 
             data.Install_rate, 
             marital_status[data.Marital_status],
-            co_applicant[data.Co_applicant],
-            data.Present_Resident,
+            # co_applicant[data.Co_applicant],
+            # data.Present_Resident,
             real_estate[data.Real_Estate],
             data.Age,
             other_installment[data.Other_installment],
-            residence[data.Residence],
+            # residence[data.Residence],
             data.Num_Credits,
             job[data.Job],
-            data.No_dependents,
+            # data.No_dependents,
             phone[data.Phone],
             foreign[data.Foreign]
             ]]
@@ -89,7 +89,8 @@ def home():
 @app.post("/predict")
 def predict(data: InputData):
     input_data = mapper(data)
-    columns = ['CHK_ACCT','Duration','History','Purpose of credit','Credit Amount','Balance in Savings A/C','Employment','Install_rate','Marital status','Co-applicant','Present Resident','Real Estate','Age','Other installment','Residence','Num_Credits','Job','No. dependents','Phone','Foreign']
+    # columns = ['CHK_ACCT','Duration','History','Purpose of credit','Credit Amount','Balance in Savings A/C','Employment','Install_rate','Marital status','Co-applicant','Present Resident','Real Estate','Age','Other installment','Residence','Num_Credits','Job','No. dependents','Phone','Foreign']
+    columns = ['CHK_ACCT','Duration','History','Purpose of credit','Credit Amount','Balance in Savings A/C','Employment','Install_rate','Marital status','Real Estate','Age','Other installment','Num_Credits','Job','Phone','Foreign']
     credit = {1:'good', 0:'bad'}
     new_df = pd.DataFrame(input_data, columns=columns)
     try:
